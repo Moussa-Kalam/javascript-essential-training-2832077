c988c5feee91be55e57fb24931ba456a86b9dc4f
Moussa-Kalam <m.amzat@alustudent.com>
2022-11-21 00:52:07 +0200

    Building objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -14,9 +14,19 @@ const laptop = {
   isNew: false,
   user: 2,
   specifications: {
-    RAM: 8,
+    RAM: "8 gb",
     ROM: "1 To",
     keyboard: "AZERTY",
     charger: 2,
   },
 };
+
+console.log("The laptop object", laptop);
+
+// Accessing properties using dot notation
+console.log("My laptop's brand is", laptop.brand);
+console.log("My laptop's specifications:", laptop.specifications);
+
+// Accessing properties using bracket notations
+console.log("My laptop's RAM is:", laptop["specifications"]["RAM"]);
+console.log("Number of chargers:", laptop["specifications"]["charger"]);
