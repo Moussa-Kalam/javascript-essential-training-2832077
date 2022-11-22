d0d747a8ac56d70162466e93062481586cb03358
Moussa-Kalam <m.amzat@alustudent.com>
2022-11-22 17:47:38 +0200

    Modifiying classes, attributes and styles

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -1,5 +1,31 @@
-/**
- * Note: This file is intentionally empty.
- * You can use it to test your skills at adding, removing, or modifying
- * attributes, classes, and inline styles on elements.
- */
+// Find an element and add two different classes
+document.querySelector(".maincontent").classList.add("start");
+document.querySelector(".maincontent").classList.add("end");
+
+// Remove only one of the new classes
+document.querySelector(".maincontent").classList.remove("end");
+
+// Add a new attribute to an element
+document.querySelector("footer").setAttribute("id", "sitefooter");
+
+//Request the value of the attribute added
+let value = document.querySelector("footer").getAttribute("id");
+console.log(value);
+
+// Change the value of the attribute
+document.querySelector("footer").setAttribute("id", "end");
+
+/* 
+Add an inline CSS to an element by specifying
+the style property and setting its value
+*/
+document.querySelector(".site-title").style.backgroundColor = "green";
+document.querySelector(".site-title").style.color = "white";
+document.querySelector(".site-description").style.color = "red";
+document.querySelector(".backpack__name").style.backgroundColor = "yellow";
+
+/* 
+Query the style property of an element in the console to 
+get a list of all the style properties available.
+*/
+console.log(document.querySelector("main").style);
